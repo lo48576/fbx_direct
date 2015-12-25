2c26491942d1dfcfe0cebaf544b67c3d1b892ba2
Takuma Yoshioka <nu11p0.6477@gmail.com>
2015-12-25 14:05:49 +0900

    Add `reader::Events` and related functions and types
    In `reader` module, added
    - FbxEvent
    - FbxFormatType
    - Events
    and implemented some traits and member functions.

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,10 +3,21 @@ extern crate fbx_direct;
 use std::fs::File;
 use std::io::BufReader;
 
-use fbx_direct::reader::{EventReader};
+use fbx_direct::reader::{EventReader, FbxEvent};
 
 fn main() {
     let file = BufReader::new(File::open("sample.fbx").unwrap());
 
-    let _parser = EventReader::new(file);
+    let parser = EventReader::new(file);
+    for e in parser {
+        match e {
+            Err(e) => {
+                println!("Error: {:?}", e);
+                break;
+            },
+            _ => {
+                println!("{:?}", e);
+            }
+        }
+    }
 }
--- src/reader/mod.rs
+++ src/reader/mod.rs
@@ -1,4 +1,23 @@
 use std::io::Read;
+use error::{Error, ErrorKind};
+use ::Result;
+
+#[derive(Debug, Clone, Copy)]
+pub enum FbxFormatType {
+    Binary(u32),
+    Text,
+}
+
+#[derive(Debug, Clone)]
+pub enum FbxEvent {
+    StartFbx(FbxFormatType),
+    EndFbx,
+    StartNode {
+        name: String,
+    },
+    EndNode,
+    Comment(String),
+}
 
 pub struct EventReader<R: Read> {
     source: R,
@@ -8,6 +27,44 @@ impl<R: Read> EventReader<R> {
     pub fn new(source: R) -> Self {
         EventReader {
             source: source,
+        }
+    }
+    pub fn next(&mut self) -> Result<FbxEvent> {
+        Err(Error::new(0, ErrorKind::Unimplemented("EventReader is not implemented yet".to_string())))
+    }
+}
+
+impl <R: Read> IntoIterator for EventReader<R> {
+    type Item = Result<FbxEvent>;
+    type IntoIter = Events<R>;
+
+    fn into_iter(self) -> Events<R> {
+        Events {
+            reader: self,
+            finished: false,
+        }
+    }
+}
+
+/// An iterator over FBX events created from some type implementing `Read`.
+pub struct Events<R: Read> {
+    reader: EventReader<R>,
+    finished: bool,
+}
+
+impl<R: Read> Iterator for Events<R> {
+    type Item = Result<FbxEvent>;
+
+    fn next(&mut self) -> Option<Result<FbxEvent>> {
+        if self.finished {
+            None
+        } else {
+            let ev = self.reader.next();
+            match ev {
+                Ok(FbxEvent::EndFbx) | Err(_) => self.finished = true,
+                _ => {}
+            }
+            Some(ev)
         }
     }
 }
