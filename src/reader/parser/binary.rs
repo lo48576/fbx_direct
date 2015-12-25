3e525ed18da10a842ebe0df3bcb2a09480b543b5
Takuma Yoshioka <nu11p0.6477@gmail.com>
2015-12-25 23:17:36 +0900

    Add struct `NodeRecordHeader`
    NodeRecordHeader is a header and end marker of every FBX nodes.

--- src/reader/parser/binary.rs
+++ src/reader/parser/binary.rs
@@ -15,7 +15,22 @@ impl BinaryParser {
         }
     }
 
-    pub fn next<R: Read>(&mut self, _reader: &mut R, common: &mut CommonState) -> Result<FbxEvent> {
+    pub fn next<R: Read>(&mut self, reader: &mut R, common: &mut CommonState) -> Result<FbxEvent> {
+        let node_record_header = try!(NodeRecordHeader::load(reader, &mut common.pos));
         Err(Error::new(common.pos, ErrorKind::Unimplemented("Parser for Binary FBX format is not implemented yet".to_string())))
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct NodeRecordHeader {
+    end_offset: u32,
+    num_properties: u32,
+    property_list_len: u32,
+    name_len: u8,
+}
+
+impl NodeRecordHeader {
+    pub fn load<R: Read>(_reader: &mut R, pos: &mut u64) -> Result<Self> {
+        Err(Error::new(*pos, ErrorKind::Unimplemented("Parser for NodeRecordHeader is not implemented yet".to_string())))
     }
 }
