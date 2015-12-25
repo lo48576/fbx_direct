d21ed669986a4eaaa5c6cb59f23b47ea02cda548
Takuma Yoshioka <nu11p0.6477@gmail.com>
2015-12-26 01:44:02 +0900

    Now `example/simple.rs` prints events with indent

--- examples/simple.rs
+++ examples/simple.rs
@@ -6,20 +6,35 @@ use std::io::BufReader;
 
 use fbx_direct::reader::{EventReader, FbxEvent};
 
+fn indent(size: usize) -> String {
+    const INDENT: &'static str = "    ";
+    (0..size).map(|_| INDENT)
+        .fold(String::with_capacity(size * INDENT.len()), |r, s| r + s)
+}
+
 fn main() {
     env_logger::init().unwrap();
 
     let file = BufReader::new(File::open("sample.fbx").unwrap());
 
     let parser = EventReader::new(file);
+    let mut depth = 0;
     for e in parser {
         match e {
             Err(e) => {
                 println!("Error: {:?}", e);
                 break;
             },
+            Ok(ref e@FbxEvent::StartNode { .. }) => {
+                println!("{}{:?}", indent(depth), e);
+                depth += 1;
+            },
+            Ok(ref e@FbxEvent::EndNode) => {
+                depth -= 1;
+                println!("{}{:?}", indent(depth), e);
+            },
             _ => {
-                println!("{:?}", e);
+                println!("{}{:?}", indent(depth), e);
             }
         }
     }
