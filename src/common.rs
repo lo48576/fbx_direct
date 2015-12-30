160309d2ea16e1291157224cec41352741c093bf
Takuma Yoshioka <nu11p0.6477@gmail.com>
2015-12-30 15:31:40 +0900

    Add `common::Property` type for writer
    `OwnedProperty` has owned value, but `Property` has borrowed pointers
    for vector types.

--- src/common.rs
+++ src/common.rs
@@ -41,3 +41,36 @@ pub enum OwnedProperty {
     /// Raw binary data.
     Binary(Vec<u8>),
 }
+
+/// A property type of the FBX node.
+#[derive(Debug, Clone, PartialEq)]
+pub enum Property<'a> {
+    /// Boolean.
+    Bool(bool),
+    /// 2 byte signed integer.
+    I16(i16),
+    /// 4 byte signed integer.
+    I32(i32),
+    /// 8 byte signed integer.
+    I64(i64),
+    /// 4 byte single-precision IEEE 754 floating-point number.
+    F32(f32),
+    /// 8 byte double-precision IEEE 754 floating-point number.
+    F64(f64),
+    /// Array of boolean.
+    VecBool(&'a [bool]),
+    /// Array of 4 byte signed integer.
+    VecI32(&'a [i32]),
+    /// Array of 8 byte signed integer.
+    VecI64(&'a [i64]),
+    /// Array of 4 byte single-precision IEEE 754 number.
+    VecF32(&'a [f32]),
+    /// Array of 8 byte double-precision IEEE 754 number.
+    VecF64(&'a [f64]),
+    /// String.
+    ///
+    /// Note that the string can contain special character like `\u{0}`.
+    String(&'a str),
+    /// Raw binary data.
+    Binary(&'a [u8]),
+}
