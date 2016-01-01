dd0b2d16096b6a2f1a65eb759897e73840acafc7
Takuma Yoshioka <nu11p0.6477@gmail.com>
2016-01-01 17:34:32 +0900

    Improve an example

--- examples/import-export-binary.rs
+++ examples/import-export-binary.rs
@@ -6,7 +6,7 @@ extern crate env_logger;
 use std::fs::File;
 use std::io::BufReader;
 
-use fbx_direct::reader;
+use fbx_direct::reader::FbxEvent as ReaderEvent;
 use fbx_direct::reader::EventReader;
 use fbx_direct::writer::EventWriter;
 
@@ -29,70 +29,34 @@ fn main() {
             std::process::exit(1);
         },
     };
+    let new_filename = filename.clone() + ".export.fbx";
 
-
-    let mut exported1 = std::io::Cursor::new(Vec::<u8>::new());
-    {
-        let file = BufReader::new(File::open(filename.clone()).unwrap());
-        let parser = EventReader::new(file);
-        let mut emitter = EventWriter::new(exported1.by_ref());
-        let mut depth = 0;
-        for e in parser {
-            match e {
-                Ok(ref e@reader::FbxEvent::StartNode { .. }) => {
-                    debug!("{}{:?}", indent(depth), e);
-                    depth += 1;
-                },
-                Ok(ref e@reader::FbxEvent::EndNode) => {
-                    depth -= 1;
-                    debug!("{}{:?}", indent(depth), e);
-                },
-                Ok(ref e) => {
-                    debug!("{}{:?}", indent(depth), e);
-                },
-                Err(e) => {
-                    debug!("Error: {:?}", e);
-                    break;
-                },
-            }
-            if let Ok(ref e) = e {
-                emitter.write(e.as_writer_event()).unwrap();
-            }
+    let file = BufReader::new(File::open(filename.clone()).unwrap());
+    let parser = EventReader::new(file);
+    let mut emitter = EventWriter::new(File::create(new_filename.clone()).unwrap());
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(ref e@ReaderEvent::StartNode { .. }) => {
+                debug!("{}{:?}", indent(depth), e);
+                depth += 1;
+            },
+            Ok(ref e@ReaderEvent::EndNode) => {
+                depth -= 1;
+                debug!("{}{:?}", indent(depth), e);
+            },
+            Ok(ref e) => {
+                debug!("{}{:?}", indent(depth), e);
+            },
+            Err(e) => {
+                debug!("Error: {:?}", e);
+                break;
+            },
         }
-    }
-    {
-        use std::io::{Seek,SeekFrom};
-        exported1.seek(SeekFrom::Start(0)).unwrap();
-    }
-
-    let mut exported2 = std::io::Cursor::new(Vec::<u8>::new());
-    {
-        let parser = EventReader::new(exported1.by_ref());
-        let mut emitter = EventWriter::new(exported2.by_ref());
-        let mut depth = 0;
-        for e in parser {
-            match e {
-                Ok(ref e@reader::FbxEvent::StartNode { .. }) => {
-                    debug!("{}{:?}", indent(depth), e);
-                    depth += 1;
-                },
-                Ok(ref e@reader::FbxEvent::EndNode) => {
-                    depth -= 1;
-                    debug!("{}{:?}", indent(depth), e);
-                },
-                Ok(ref e) => {
-                    debug!("{}{:?}", indent(depth), e);
-                },
-                Err(e) => {
-                    debug!("Error: {:?}", e);
-                    break;
-                },
-            }
-            if let Ok(ref e) = e {
-                emitter.write(e.as_writer_event()).unwrap();
-            }
+        if let Ok(ref e) = e {
+            emitter.write(e.as_writer_event()).unwrap();
         }
     }
 
-    assert_eq!(exported1.get_ref(), exported2.get_ref());
+    println!("written to {}", new_filename);
 }
