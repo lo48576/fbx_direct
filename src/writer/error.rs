d600a40eb2629d3207319a9abde643fb09206f40
Takuma Yoshioka <nu11p0.6477@gmail.com>
2016-01-01 17:53:44 +0900

    Implement `Error` and `Display` trait for `writer::error::Error`

--- src/writer/error.rs
+++ src/writer/error.rs
@@ -3,6 +3,8 @@
 extern crate byteorder;
 
 use std::io;
+use std::fmt;
+use std::error;
 
 /// A specialized `std::result::Result` type for FBX exporting.
 pub type Result<T> = ::std::result::Result<T, Error>;
@@ -24,6 +26,41 @@ pub enum Error {
     UnwritableEvent,
     /// Unimplemented feature.
     Unimplemented(String),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::Io(ref err) => write!(f, "I/O error: {}", err),
+            Error::ExtraEndNode => write!(f, "Extra end-of-node marker detected"),
+            Error::FbxNotStarted => write!(f, "An writer event is given, but FBX data is not started yet"),
+            Error::FbxAlreadyStarted => write!(f, "Got a writer event to start FBX, but FBX data is already started"),
+            Error::UnsupportedFbxVersion => write!(f, "Unsupported FBX version"),
+            Error::UnwritableEvent => write!(f, "A given event is not writable in current format"),
+            Error::Unimplemented(ref err) => write!(f, "Unimplemented feature: {}", err),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::Io(ref err) => err.description(),
+            Error::ExtraEndNode => "Extra end-of-node marker detected",
+            Error::FbxNotStarted => "An writer event is given, but FBX data is not started yet",
+            Error::FbxAlreadyStarted => "Got a writer event to start FBX, but FBX data is already started",
+            Error::UnsupportedFbxVersion => "Unsupported FBX version",
+            Error::UnwritableEvent => "A given event is not writable in current format",
+            Error::Unimplemented(_) => "Attempt to use unimplemented feature",
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            Error::Io(ref err) => Some(err as &error::Error),
+            _ => None,
+        }
+    }
 }
 
 impl Clone for Error {
